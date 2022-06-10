4da54b6778104c9ca83d56a382b91140269eba37
iamsajith <sajithj03@gmail.com>
2022-06-10 13:37:02 +0530

    final commit

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { TestimonyService } from '../testimony.service';
 import { TestimonyModel } from './data.model';
@@ -11,12 +12,12 @@ import { TestimonyModel } from './data.model';
 export class HomeComponent implements OnInit {
 
   constructor(public TestimonyService:TestimonyService) { }
-  testimony : TestimonyModel[] | any
+  testimony : TestimonyModel[] | any;
 
   ngOnInit(): void {
     this.TestimonyService.getData().subscribe((data)=>{
       this.testimony = JSON.parse(JSON.stringify(data))
-      console.log(this.testimony[0].testimony)
+      // console.log(this.testimony[0].testimony)
     })
   }
 
